ff6af9f6b39f1b368cdf6c470b1dab65609765e4
Sudhakaran <SUDHARSANA.SUDHAKARAN@Chubb.com>
2020-07-07 11:27:27 +0530

    commit

--- src/app/formpage/formpage.component.ts
+++ src/app/formpage/formpage.component.ts
@@ -1,35 +1,34 @@
-
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-// import { DataService } from '../data.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-formpage',
   templateUrl: './formpage.component.html',
   styleUrls: ['./formpage.component.css']
 })
+
 export class FormpageComponent implements OnInit {
-  // iter: number= 0;
-  // myForm: FormGroup;
-  // productList: [];
-  // constructor(private fb: FormBuilder ) { 
-  //   this.myForm= this.fb.group({
-  //     'name': this.fb.control('', Validators.required),
-  //     'price': this.fb.control("", Validators.required),
-  //     'stock': this.fb.control("", Validators.required)
-  //   })
-  //   console.log(fb)
-  // }
+  myForm: FormGroup;
+  productList: [];
+
+  constructor()
+  {
+    this.myForm = new FormGroup(
+      {
+        name : new FormControl('', Validators.required),
+        price : new FormControl("", Validators.required),
+        stock : new FormControl("", Validators.required)
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
 
-  // submit(){
-  //   console.log(this.myForm.value.name,  this.myForm.value.price, this.myForm.value.stock);
-  //   window.localStorage.setItem(this.productForm.value.id, JSON.stringify({ name: this.productForm.value.name, price: this.productForm.value.price, stock: this.productForm.value.stock }))
-  //   this.dataService.sharedData.push(this.productForm.value.id);
-  //   this.show = false;
-
-  //   }
-  // }
+  submit(){
+    console.log(this.myForm.value.name);
+    if(this.myForm.valid)
+      localStorage.setItem('tableValues', JSON.stringify({name : this.myForm.value.name, price : this.myForm.value.price, stock : this.myForm.value.stock}));
+    
+  }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -12,9 +12,16 @@ export class TableComponent implements OnInit {
   constructor() { 
    // this.temp = shared.getOption()
     this.dataList = []
-    for(let ts in this.temp){
-      console.log({name:ts, price:this.temp[ts].price, stock: this.temp[ts].stock})
-      this.dataList.push({name:ts, price:this.temp[ts].price, stock: this.temp[ts].stock})
+    // for(let ts in this.temp){
+    //   console.log({name:ts, price:this.temp[ts].price, stock: this.temp[ts].stock})
+    //   this.dataList.push({name:ts, price:this.temp[ts].price, stock: this.temp[ts].stock})
+    // }
+    if(localStorage.getItem('tableValues') ===  null)
+    {
+      this.dataList = [];
+    }
+    else{
+      this.dataList.push(JSON.parse(localStorage.getItem('tableValues')));
     }
     // for(let i= 0; i< ; i++){
     //   this.dataList.push(JSON.parse(localStorage.getItem(i.toString())))
